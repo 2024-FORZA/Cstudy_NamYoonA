6cc9c624c5cd9f496c82ecadd3d537d740d68ebd
yoonkong1208 <yoona120808@gmail.com>
2024-09-13 09:15:09 +0900

    2_Lab01_4.c

---
+++ 2_Lab01_4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int N, M, A[100][100], B[100][100];
+    scanf("%d %d", &N, &M);
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            scanf("%d", &A[i][j]);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            scanf("%d", &B[i][j]);
+            printf("%d ", A[i][j] + B[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
