33b6e551407c7ab0b156ed3a0601e2a13f53f5f7
yoonkong1208 <yoona120808@gmail.com>
2024-10-16 17:59:29 +0900

    2_Lab05_new4.c

--- 2_Lab05_4.c
+++ 2_Lab05_4.c
@@ -1,13 +1,33 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 
 int main() {
-    int M, cup = 1, X, Y;
-    scanf("%d", &M);
-    while (M--) {
-        scanf("%d %d", &X, &Y);
-        if (X == cup) cup = Y;
-        else if (Y == cup) cup = X;
+    while (1) {
+        char inputString[101] = { 0, };  // 문자열 입력 배열
+        scanf("%[^\n]s", inputString);  // 개행 문자 전까지 입력받음
+
+        // 빈 줄이 입력되면 종료
+        if (inputString[0] == '\0')
+            break;
+
+        int lowercaseCount = 0, uppercaseCount = 0, digitCount = 0, spaceCount = 0;
+
+        // 문자열의 각 문자를 순회하며 카운트
+        for (int i = 0; i < strlen(inputString); i++) {
+            if (inputString[i] == 32) // 공백
+                spaceCount++;
+            else if ((48 <= inputString[i]) && (inputString[i] <= 57)) // 숫자
+                digitCount++;
+            else if ((65 <= inputString[i]) && (inputString[i] <= 90)) // 대문자
+                uppercaseCount++;
+            else if ((97 <= inputString[i]) && (inputString[i] <= 122)) // 소문자
+                lowercaseCount++;
+        }
+
+        // 결과 출력
+        printf("%d %d %d %d\n", lowercaseCount, uppercaseCount, digitCount, spaceCount);
+        scanf("%*c");  // 개행 문자 제거
     }
-    printf("%d\n", cup);
+
     return 0;
 }
